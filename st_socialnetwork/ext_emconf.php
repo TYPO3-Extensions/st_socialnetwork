26fb2dfd7ecb755659d3a963ac178badf43abd10
scheibo <scheibo@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-07-22 13:05:22 +0000

    first upload in the SVN
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/st_socialnetwork/trunk@64742 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ st_socialnetwork/ext_emconf.php
@@ -0,0 +1,39 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "st_socialnetwork".
+#
+# Auto generated 01-06-2012 03:00
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Social Network - Recommend link',
+	'description' => 'With this extension, you are able to recommend your website and news by using social networks (Twitter, Facebook, LinkedIn, Xing, YouTube, MeinVz, Tumblr, VKontakte, Flickr).',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '1.4.1',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'TYPO3_version' => '',
+	'PHP_version' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Thomas Scheibitz',
+	'author_email' => 'mail@kreativschmiede-eichsfeld.de',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+);
+
+?>
